d76bb34bfc0ad335556491427ea8b316b7e874ac
biglolo004 <146000255+LotfiBensada@users.noreply.github.com>
2025-09-01 02:31:10 -0400

    Initial layout of the application

---
+++ RhytmixReload/Interfaces/ISong.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RhytmixReload.Interfaces
+{
+    public interface ISong
+    {
+        string Name { get; }
+        string Artist { get; }
+        string Description { get; }
+        int Likes { get; set; }
+        string length {  get; }
+
+    }
+}
